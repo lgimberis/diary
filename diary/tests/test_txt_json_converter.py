c7b610d201b15afbc27733c5b3600f92bf2aa162
L Gimberis <lukasgimberis@gmail.com>
2021-04-17 18:36:11 +0100

    Add __init__.py. Update test. Add 4 basic tests

--- diary/tests/test_txt_json_converter.py
+++ diary/tests/test_txt_json_converter.py
@@ -1,16 +1,26 @@
 
 import unittest
-from io import StringIO as si
+from glob import glob
+import os
+import json
+from io import StringIO as SI
 from ..txt_json_converter import txt_to_json
 
 class TestTxtToJSON(unittest.TestCase):
-    def testSimple(self):
-        dummyFile = si("[Dummy Key]\nDummy Value")
-        self.assertEqual(txt_to_json(dummyFile), {"Dummy Key": {"none": "Dummy Value"}})
-    def testCategory(self):
-        dummyFile = si("[Dummy Key]\n(Dummy Category)\nDummy Value")
-        self.assertEqual(txt_to_json(dummyFile), 
-            {"Dummy Key": {"Dummy Category": "Dummy Value"}})
+    def testDatabase(self):
+        '''Test each set of files matching ./data/txt_json_converter/t2j* with txt_to_json
+        '''
+        dataDirectory = os.path.join(os.path.dirname(__file__), "data", "txt_json_converter")+os.sep
+        inputFiles = glob(dataDirectory+"t2j_*input.txt")
+        resultFiles = glob(dataDirectory+"t2j_*result.json")
+
+        self.assertEqual(len(inputFiles), len(resultFiles), "Ensure there is a result file for each input")
+        for inputFilename, resultFilename in zip(inputFiles, resultFiles):
+            with open(inputFilename) as inputFile:
+                output = txt_to_json(inputFile)
+            with open(resultFilename) as resultFile:
+                result = json.load(resultFile)
+            self.assertEqual(output, result, f"\nOutput from {inputFilename} does not match {resultFilename}")
 
 def main():
     unittest.main()
