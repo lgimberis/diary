f6745f880a55c514bc5f284d583916c7dbc6202f
L Gimberis <lukasgimberis@gmail.com>
2021-04-17 19:53:28 +0100

    Rename all tests, modify test accordingly. Use subTests for each entry.

--- diary/tests/test_txt_json_converter.py
+++ diary/tests/test_txt_json_converter.py
@@ -11,16 +11,17 @@ class TestTxtToJSON(unittest.TestCase):
         '''Test each set of files matching ./data/txt_json_converter/t2j* with txt_to_json
         '''
         dataDirectory = os.path.join(os.path.dirname(__file__), "data", "txt_json_converter")+os.sep
-        inputFiles = glob(dataDirectory+"t2j_*input.txt")
-        resultFiles = glob(dataDirectory+"t2j_*result.json")
+        inputFiles = glob(dataDirectory+"*.txt")
+        resultFiles = glob(dataDirectory+"*.json")
 
         self.assertEqual(len(inputFiles), len(resultFiles), "Ensure there is a result file for each input")
         for inputFilename, resultFilename in zip(inputFiles, resultFiles):
-            with open(inputFilename) as inputFile:
-                output = txt_to_json(inputFile)
-            with open(resultFilename) as resultFile:
-                result = json.load(resultFile)
-            self.assertEqual(output, result, f"\nOutput from {inputFilename} does not match {resultFilename}")
+            with self.subTest(inputFilename=inputFilename, resultFilename=resultFilename):
+                with open(inputFilename) as inputFile:
+                    output = txt_to_json(inputFile)
+                with open(resultFilename) as resultFile:
+                    result = json.load(resultFile)
+                self.assertEqual(output, result, f"\nOutput from {inputFilename} does not match {resultFilename}")
 
 def main():
     unittest.main()
