ef168e0d2ae3dc7504f0a4ff64777e7d92b4cbb5
L Gimberis <lukasgimberis@gmail.com>
2021-04-25 16:24:47 +0100

    Add JSON to TXT and tests

--- diary/tests/test_txt_json_converter.py
+++ diary/tests/test_txt_json_converter.py
@@ -3,25 +3,46 @@ import unittest
 from glob import glob
 import os
 import json
+import re
 from io import StringIO as SI
-from ..txt_json_converter import txt_to_json
+from ..txt_json_converter import txt_to_json, json_to_txt
 
 class TestTxtToJSON(unittest.TestCase):
     def testDatabase(self):
-        '''Test each set of files matching ./data/txt_json_converter/t2j* with txt_to_json
+        '''Confirm that the output of txt_to_json for each txt file in test database matches expected JSON output
         '''
-        dataDirectory = os.path.join(os.path.dirname(__file__), "data", "txt_json_converter")+os.sep
-        inputFiles = glob(dataDirectory+"*.txt")
-        resultFiles = glob(dataDirectory+"*.json")
+        dataDirectory = os.path.join(os.path.dirname(__file__), "data", "txt_json_converter")
+        txtFiles = glob(os.path.join(dataDirectory,"*.txt"))
+        jsonFiles = glob(os.path.join(dataDirectory,"*.json"))
 
-        self.assertEqual(len(inputFiles), len(resultFiles), "Ensure there is a result file for each input")
-        for inputFilename, resultFilename in zip(inputFiles, resultFiles):
-            with self.subTest(inputFilename=inputFilename, resultFilename=resultFilename):
-                with open(inputFilename) as inputFile:
-                    output = txt_to_json(inputFile)
-                with open(resultFilename) as resultFile:
-                    result = json.load(resultFile)
-                self.assertEqual(output, result, f"\nOutput from {inputFilename} does not match {resultFilename}")
+        self.assertEqual(len(txtFiles), len(jsonFiles), "Ensure there is a json file for each txt")
+        for txtFilename, jsonFilename in zip(txtFiles, jsonFiles):
+            with self.subTest(txtFilename=txtFilename, jsonFilename=jsonFilename):
+                with open(txtFilename) as txtFile:
+                    output = txt_to_json(txtFile)
+                with open(jsonFilename) as jsonFile:
+                    jsonOutput = json.load(jsonFile)
+                self.assertEqual(output, jsonOutput, f"\nOutput from {txtFilename} does not match {jsonFilename}\n{output}\n{jsonOutput}")
+
+class TestJsonToTxt(unittest.TestCase):
+    def testDatabase(self):
+        '''Confirm that the output of json_to_txt for each JSON file in test database matches expected txt output
+        '''
+        dataDirectory = os.path.join(os.path.dirname(__file__), "data", "txt_json_converter")
+        txtFiles = glob(os.path.join(dataDirectory,"*.txt"))
+        jsonFiles = glob(os.path.join(dataDirectory,"*.json"))
+
+        self.assertEqual(len(txtFiles), len(jsonFiles), "Ensure there is a txt file for each JSON")
+        for txtFilename, jsonFilename in zip(txtFiles, jsonFiles):
+            with self.subTest(txtFilename=txtFilename, jsonFilename=jsonFilename):
+                with open(jsonFilename) as jsonFile:
+                    output = re.sub('\n','',json_to_txt(jsonFile))
+                with open(txtFilename) as txtFile:
+                    txtOutput = ''
+                    for line in txtFile:
+                        txtOutput += line
+                    txtOutput = re.sub('\n','',txtOutput)
+                self.assertEqual(output, txtOutput, f"\nOutput from {jsonFilename} does not match {txtFilename}")
 
 def main():
     unittest.main()
--- diary/txt_json_converter.py
+++ diary/txt_json_converter.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 class NonComformingTextException(Exception):
     '''Raised when a .txt file does not match the expected format'''
@@ -22,28 +23,60 @@ def txt_to_json(f):
         if line.strip():
             match = re.match(r'\[([^\]]*)\]', line.strip())
             if match:
-                if key:
-                    myJson[key][category] = value
+                if key and value:
+                    try:
+                        myJson[key][category] = value
+                    except KeyError:
+                        myJson[key] = {category:value}
                 key = match.group(1)
 
                 if key in myJson:
                     raise NonComformingTextException(f'Key {key} declared multiple times')
                 category = "none"
-                myJson[key] = {}
                 value = ''
             else:
                 matchCategory = re.match(r'\(([^\)]*)\)', line.strip())
                 if matchCategory:
+                    if value:
+                        try:
+                            myJson[key][category] = value
+                        except KeyError:
+                            myJson[key] = {category:value}
+                        value = ''
                     category = matchCategory.group(1)
                     if category == "none":
                         raise NonComformingTextException(f'"none" is a reserved category name')
-                    elif category in myJson[key]:
+                    elif key in myJson and category in myJson[key]:
                         raise NonComformingTextException(f'Category {category} declared multiple times')
                 else:
                     if key:
+                        if value:
+                            value += '\n'
                         value += line.strip()
                     else:
                         raise NonComformingTextException('Key not declared before text')
-    if key:
-        myJson[key][category] = value
+    if key and value:
+        try:
+            myJson[key][category] = value
+        except KeyError:
+            myJson[key] = {category: value}
     return myJson
+
+def json_to_txt(f):
+    '''Convert an existing JSON file to a .txt file in our special format
+    '''
+
+    txtFile = ''
+    fileDict = json.load(f)
+    for key in fileDict:
+        if txtFile:
+            txtFile += '\n'
+        txtFile += '['+key+']'
+        thisKey = fileDict[key]
+        if "none" in thisKey:
+            txtFile += '\n'+thisKey["none"]
+        for category in thisKey:
+            if category != "none":
+                txtFile += '\n('+category+')'
+                txtFile += '\n'+thisKey[category]
+    return txtFile
