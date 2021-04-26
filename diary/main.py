bba4d8336aa748393795577a86afef73f6d14fd3
L Gimberis <lukasgimberis@gmail.com>
2021-04-26 09:54:44 +0100

    Add a small Command class to avoid ugly indexing within CLI body

--- diary/main.py
+++ diary/main.py
@@ -4,24 +4,34 @@ import sys
 
 from .diary import Diary
 
+class Command:
+    def __init__(self, aliases, help, function, numargs):
+        self.aliases = aliases
+        self.help = help
+        self.function = function
+        self.numargs = numargs
+
 def main():
     """Use a CLI to the Diary module.
     """
 
+    HELP_NAME = "Help"
+    EXIT_NAME = "Exit"
+
     COMMANDS = OrderedDict[
-        ("Help", [["help", "h"], "Show this help", None, None]),
-        ("Exit", [["exit", "e"], "Stop Execution", None, None]),
-        ("Today", [["today", "t"], "Open today's entry", "today", 0]),
-        ("Date", [["date", "d"], "Open an entry corresponding to the given date", "get_entry", 1]),
-        ("Open", [["open", "o"], "Open a file of the given name", "get_file", 1]),
+        (HELP_NAME, Command(["help", "h"], "Show this help", None, None)),
+        (EXIT_NAME, Command(["exit", "e"], "Stop Execution", None, None)),
+        ("Today", Command(["today", "t"], "Open today's entry", "today", 0)),
+        ("Date", Command(["date", "d"], "Open an entry corresponding to the given date", "get_entry", 1)),
+        ("Open", Command(["open", "o"], "Open a file of the given name", "get_file", 1)),
     ]
 
     def help():
         """List all the commands.
         """
         for command in COMMANDS:
-            alias_list = str(COMMANDS[command][0])[1:-1] #Remove '[' and ']' from string representation
-            print(f"{command} ({alias_list}) - {COMMANDS[command][1]}")
+            alias_list = str(COMMANDS[command].aliases)[1:-1] #Remove '[' and ']' from string representation
+            print(f"{command} ({alias_list}) - {COMMANDS[command].help}")
 
     if len(sys.argv) > 1:
         root = sys.argv[1]
@@ -29,7 +39,7 @@ def main():
         root = os.getcwd()
     with Diary(root) as d:
         main_command = None
-        while main_command not in COMMANDS["Exit"][0]:
+        while main_command not in COMMANDS[EXIT_NAME].aliases:
             response = [word for word in input("Enter command:\n").lower().split(" ") if word]
             call_found = False
             print_help = False
@@ -37,16 +47,16 @@ def main():
             if len(response > 0):
                 main_command = response[0]
                 for command in COMMANDS:
-                    if main_command in COMMANDS[command][0]:
+                    if main_command in COMMANDS[command].aliases:
                         call_found = True
-                        if command == "Help":
+                        if command == HELP_NAME:
                             print_help = True
                         if COMMANDS[command][2]:
-                            if len(response) == 1+COMMANDS[command][3]:
-                                func = getattr(d, COMMANDS[command][2])
+                            if len(response) == 1+COMMANDS[command].numargs:
+                                func = getattr(d, COMMANDS[command].function)
                                 func(*response[1:])
                             else:
-                                print(f"Expected {COMMANDS[command][3]} arguments, got {len(response)-1}")
+                                print(f"Expected {COMMANDS[command].numargs} arguments, got {len(response)-1}")
             else:
                 main_command = None
             if not call_found:
