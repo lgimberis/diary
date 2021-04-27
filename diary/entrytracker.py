4ff9fca31c751ba3780d76e04b73210d26caa316
L Gimberis <lukasgimberis@gmail.com>
2021-04-27 18:37:26 +0100

    Expand test suite. Add functionality to properly handle updated files

--- diary/entrytracker.py
+++ diary/entrytracker.py
@@ -2,16 +2,29 @@
 TOTALS_KEY_NAME = "_Totals"
 
 class EntryCategory:
+    """Base class for tracking metadata.
+
+    EntryCategory objects have a list of contributors which each contribute
+    an amount of characters.
+    The total over all contributors is kept in self.length.
+    """
     def __init__(self):
         self.contributors = {}
         self.length = 0
 
     def add_contributor(self, name, value):
-        if name in self.contributors:
-            self.contributors[name] += value
-        else:
-            self.contributors[name] = value
+        """Add given contributor, adjusting self.length accordingly.
+        """
+        self.contributors[name] = value
         self.length += value
+    
+    def remove_contributor(self, name):
+        """Remove given contributor, adjusting self.length accordingly.
+
+        """
+        value = self.contributors[name]
+        self.length -= value
+        self.contributors.pop(name)
 
     def __len__(self):
         return self.length
@@ -20,13 +33,22 @@ class EntryCategory:
         return self.contributors[key]
 
     def get_contributors(self):
+        """Get the names of all contributors in a list.
+
+        """
         return list(self.contributors.keys())
 
     def get_contributors_with_lengths(self):
+        """Get the names of all contributors and their contributions in a dict.
+
+        """
         return self.contributors
 
 
 class EntryKey(EntryCategory):
+    """EntryKey tracks collections of metadata.
+
+    """
     def __init__(self):
         self.categories = {}
         super().__init__()
@@ -42,7 +64,25 @@ class EntryKey(EntryCategory):
         if category not in self.categories:
             self.categories[category] = EntryCategory()
         self.categories[category].add_contributor(name, value)
-        super().add_contributor(name, value)
+
+        #Update contributors
+        if name in self.contributors:
+            self.contributors[name] += value
+        else:
+            self.contributors[name] = value
+        self.length += value
+
+    def remove_contributor(self, name):
+        super().remove_contributor(name)
+        toRemove = {key:False for key in self.categories}
+        for category in self.categories:
+            if name in self.categories[category].get_contributors():
+                self.categories[category].remove_contributor(name)
+                if len(self.categories[category]) == 0:
+                    toRemove[category] = True
+        for category in toRemove:
+            if toRemove[category]:
+                self.categories.pop(category)
 
     def __recalculate_length(self):
         length = 0
@@ -88,7 +128,10 @@ class EntryTracker:
         basename = ".".join(filename.split(".")[:-1])
 
         #If this file is already in the database, remove its contents first
-        #TODO
+        if basename in self.files:
+            for contribution in self.files[basename].get_contributions():
+                if contribution != TOTALS_KEY_NAME:
+                    self.keys[contribution].remove_contributor(basename)
 
         #Add the file's entry
         self.files[basename] = EntryFile(dictionary)
@@ -98,4 +141,5 @@ class EntryTracker:
             for category in dictionary[key]:
                 if key not in self.keys:
                     self.keys[key] = EntryKey()
-                self.keys[key].add_contributor(basename, len(dictionary[key][category]), category)
+                self.keys[key].add_contributor(basename, 
+                    len(dictionary[key][category]), category)
--- diary/tests/test_entrytracker.py
+++ diary/tests/test_entrytracker.py
@@ -1,58 +1,177 @@
 from io import StringIO
 import unittest
-import unittest.mock
 
 from ..entrytracker import EntryTracker
 from ..txt_dict_converter import txt_to_dict
 
+
 class EntryTrackerTester(unittest.TestCase):
-    @unittest.mock.patch('builtins.open')
-    def testAddFirstFile(self, mocked_open):
+
+    def __init__(self, *args, **kwargs):
+        self.example_file_one = """
+            [Key 1]
+            Here is some data for key 1
+            [Key 2]
+            Key 2's data
+            (Category)
+            Let's make sure the category is tracked too
+            """.replace('\t','')
+        self.example_file_two = """
+            [Key 1]
+            Here are some words without a category.
+            (Category 1)
+            Now we have a category.
+            (Category 2)
+            We have a second category in this key.
+            [Key 2]
+            We have another key.
+            [Key 3]
+            We have a third key.
+            (Category 3)
+            This key has a category too.
+            [Key 4]
+            (Category 4)
+            This key doesn't have a None category.
+            """.replace('\t','')
+        self.example_file_three = """
+            [Key 1]
+            Key 1 is back!
+            [Key 3]
+            (Category 3)
+            What if we add something to Key 3's category?
+            [Key 6]
+            (Category 5)
+            What if we add a key and a category?
+            """.replace('\t','')
+        
+        super().__init__(*args, **kwargs)
+
+    def __create_two_files(self):
+        et = EntryTracker()
+        first_file_content = self.example_file_two
+        second_file_content = self.example_file_three
+        et.add_file(txt_to_dict(StringIO(first_file_content)), "first_file_name.txt")
+        et.add_file(txt_to_dict(StringIO(second_file_content)), "second_file_name.txt")
+        return et
+
+    def testAddFirstFile(self):
         """Ensure that our first addition to an empty EntryTracker behaves as expected.
 
         """
         et = EntryTracker()
-        file_content = "[Key 1]\nHere is some data for key 1\n[Key 2]\nKey 2's data\n(Category)\nLet's make sure the category is tracked too"
-        file_as_dict = txt_to_dict(StringIO(file_content))
-        mocked_open.return_value = StringIO(file_content)
-        et.add_file(file_as_dict, "mytestfile.txt")
+        file_content = self.example_file_one
+        et.add_file(txt_to_dict(StringIO(file_content)), "mytestfile.txt")
 
         #Check that we can see our file and the keys and lengths it's added
-        self.assertEqual(et.files["mytestfile"].get_contributions(), 
-            {
-                "_Totals": {
-                    "Key 1": 27,
-                    "Key 2": 55
-                },
-                "Key 1": {
-                    None: 27
-                },
-                "Key 2": {
-                    None : 12,
-                    "Category": 43
+        with self.subTest(msg="EntryFile:get_contributions"):
+            self.assertEqual(et.files["mytestfile"].get_contributions(), 
+                {
+                    "_Totals": {
+                        "Key 1": 27,
+                        "Key 2": 55
+                    },
+                    "Key 1": {
+                        None: 27
+                    },
+                    "Key 2": {
+                        None : 12,
+                        "Category": 43
+                    }
                 }
-            }
-        )
+            )
         #Ensure that we're tracking Key 1:None, Key 2:None, and Key 2:Category
-        self.assertTrue("Key 1" in et)
-        self.assertTrue("Key 2" in et)
-        self.assertTrue("Category" in et["Key 2"])
+        with self.subTest(msg="EntryTracker:__contains__"):
+            self.assertTrue("Key 1" in et)
+            self.assertTrue("Key 2" in et)
+        with self.subTest(msg="EntryKey:__contains__"):
+            self.assertTrue("Category" in et["Key 2"])
         #Check the lengths of each
-        self.assertEqual(len(et["Key 1"]), 27)
-        self.assertEqual(len(et["Key 2"]), 55)
-        self.assertEqual(len(et["Key 2"]["Category"]), 43)
-        self.assertEqual(len(et["Key 2"][None]), 12)
+        with self.subTest(msg="EntryKey:__len__"):
+            self.assertEqual(len(et["Key 1"]), 27)
+            self.assertEqual(len(et["Key 2"]), 55)
+        with self.subTest(msg="EntryCategory:__len__"):
+            self.assertEqual(len(et["Key 2"]["Category"]), 43)
+            self.assertEqual(len(et["Key 2"][None]), 12)
         #Check that our file is mentioned in the contributors for each category
         self.assertEqual(et["Key 1"].get_contributors_with_lengths(), {"mytestfile":27})
         self.assertEqual(et["Key 2"].get_contributors_with_lengths(), {"mytestfile":55})
         self.assertEqual(et["Key 2"]["Category"].get_contributors_with_lengths(), {"mytestfile":43})
         self.assertEqual(et["Key 2"][None].get_contributors_with_lengths(), {"mytestfile":12})
 
+    def testAddTwoFiles(self):
+        """Ensure that we can add two files and have everything work correctly.
 
-    # def testAddFile(self):
-    #     """Ensure that adding files with various keys and categories works as expected.
+        """
 
-    #     """
+        et = self.__create_two_files()
+
+        with self.subTest(msg="New data for existing key"):
+            self.assertEqual(et["Key 1"].get_contributors_with_lengths(), 
+                {"first_file_name":100, "second_file_name":14})
+            self.assertEqual(et["Key 3"].get_contributors_with_lengths(),
+                {"first_file_name":48, "second_file_name":45})
+        with self.subTest(msg="New data for existing category"):
+            self.assertEqual(et["Key 3"]["Category 3"].get_contributors_with_lengths(),
+                {"first_file_name":28, "second_file_name":45})
+        with self.subTest(msg="New key and category"):
+            self.assertEqual(et["Key 6"]["Category 5"].get_contributors_with_lengths(),
+                {"second_file_name":36})
+
+    def testUpdateFile(self):
+        """Ensure that metadata updates correctly upon updating one file.
+
+        """
+
+        #As in the previous test, create a two-file database
+        #This perfectly simulates an N-file database with 1 file that we will update
+        et = self.__create_two_files()
+
+        #Make our changes to the first file
+        amended_first_file_dict = txt_to_dict(StringIO(self.example_file_two))
+
+        #Modify existing key and category
+        amended_first_file_dict["Key 1"][None] = "These words have changed."
+        #Remove single-contributor category
+        amended_first_file_dict["Key 1"].pop("Category 1")
+        #Remove existing key and category
+        amended_first_file_dict.pop("Key 3")
+        #Add a brand-new key and category
+        amended_first_file_dict["Key _"] = {"My Category":"Brand-new category text"}
+        #Add existing key
+        amended_first_file_dict["Key 6"] = {None:"Some extra text"}
+        #Add an existing key and category
+        amended_first_file_dict["Key 6"]["Category 5"] = "Extra test for existing key"
+
+        #Re-add and ensure all the changes are reflected correctly
+        et.add_file(amended_first_file_dict, "first_file_name.txt")
+
+        with self.subTest(msg="Modify existing key and category"):
+            self.assertEqual(et["Key 1"][None].get_contributors_with_lengths(),
+                {"first_file_name":25, "second_file_name":14})
+
+        with self.subTest(msg="Remove single-contributor category"):
+            self.assertFalse("Category 1" in et["Key 1"])
+
+        with self.subTest(msg="Remove existing key and category"):
+            self.assertTrue("Key 3" in et)
+            self.assertTrue("Category 3" in et["Key 3"])
+            self.assertEqual(et["Key 3"].get_contributors_with_lengths(),
+                {"second_file_name":45})
+
+        with self.subTest(msg="Add a brand-new key and category"):
+            self.assertTrue("Key _" in et)
+            self.assertTrue("My Category" in et["Key _"])
+            self.assertEqual(et["Key _"]["My Category"].get_contributors_with_lengths(),
+                {"first_file_name":23})
+
+        with self.subTest(msg="Add an existing key"):
+            self.assertTrue(None in et["Key 6"])
+            self.assertEqual(et["Key 6"].get_contributors_with_lengths(),
+                {"first_file_name":42, "second_file_name":36})
+
+        with self.subTest(msg="Add an existing key and category"):
+            self.assertEqual(et["Key 6"]["Category 5"].get_contributors_with_lengths(),
+                {"first_file_name":27, "second_file_name":36})
 
 def main():
     unittest.main()
