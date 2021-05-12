ad3d978335b60d276563db8e5e27366a6422ee22
lgimberis <lukasgimberis@gmail.com>
2021-05-12 14:55:55 +0100

    Add first mockup of config file: creation, editing, update awareness, reading, usage

--- diary/diary.py
+++ diary/diary.py
@@ -3,12 +3,13 @@ import json
 import os
 from pathlib import Path
 import platform
+import re
 import subprocess
 from getpass import getpass
 
 from diary.encrypt import Encryptor
 # from diary.entrytracker import EntryTracker
-from diary.text_dict_converter import text_filename_to_dict, json_file_to_text
+from diary.text_dict_converter import TextDictConverter
 
 
 class Diary:
@@ -25,6 +26,107 @@ class Diary:
     ]
 
     ROOT_FILE_NAME = "diary.dat"
+    CONFIG_FILE_NAME = "config.cfg"
+
+    class ConfigFile:
+        """Complete management of the config file.
+
+        ConfigFile behaves like an extended dictionary, directly supporting
+        get and set via square bracket operators.
+        """
+        CONFIG_CATEGORY_PREFIX = "Category_Prefix"
+        CONFIG_CATEGORY_SUFFIX = "Category_Suffix"
+        CONFIG_SUBCATEGORY_PREFIX = "Subcategory_Prefix"
+        CONFIG_SUBCATEGORY_SUFFIX = "Subcategory_Suffix"
+        CONFIG_USE_ENCRYPTION = "Use_Encryption"
+        CONFIG_ICS = "Internal_Category_Separator"
+
+        def defaults(self):
+            """Return the default config values.
+            """
+            return {
+                self.CONFIG_CATEGORY_PREFIX: "[",
+                self.CONFIG_CATEGORY_SUFFIX: "]",
+                self.CONFIG_SUBCATEGORY_PREFIX: "[[",
+                self.CONFIG_SUBCATEGORY_SUFFIX: "]]",
+                self.CONFIG_USE_ENCRYPTION: "True",
+                self.CONFIG_ICS: ";"
+            }
+
+        def __init__(self, path: Path, may_create=True):
+            """Set config to default.
+
+            """
+            self.config = self.defaults()
+            self.path = path
+            self.exists = path.is_file()
+            if may_create and not self.exists:
+                self.create_config_file()
+
+        def __getitem__(self, key):
+            return self.config[key]
+
+        def __setitem__(self, key, value):
+            self.config[key] = value
+
+        def get_file(self):
+            return self.path
+
+        def reload(self) -> bool:
+            """Update self.config with the new/current content of the config file.
+
+            Returns whether any config values changed.
+            This should be called whenever the file on disk could have been updated.
+            """
+            new_config = self.defaults()
+            try:
+                lines = self.path.read_text(encoding='utf-8').split("\n")
+                for line in lines:
+                    if line.strip() and (match := re.match(r"^\s*(.*)=(.*)\s*(#,^)", line)):
+                        new_config[match.group(1)] = match.group(2)
+            except IOError:
+                raise IOError(f"Config file {str(self.path)} does not exist ")
+            config_changed = self.config != new_config
+            self.config = new_config
+            return config_changed
+
+        def create_config_file(self, may_overwrite=True):
+            """Create a config file based on self.config.
+
+            If the config file already exists, this function will either overwrite
+            it, or raise an exception if not may_overwrite.
+
+            This should be called whenever self.config has been modified.
+            """
+            temp_config_file = self.path.with_suffix(self.path.suffix+"_tmp")
+            with temp_config_file.open(mode='w') as f:
+                def write_value(temp_f, value):
+                    """Helper function to write value=self.config[value]\n"""
+                    temp_f.write(f"{value}={repr(self.config[value])}\n")
+                write_value(f, self.CONFIG_CATEGORY_PREFIX)
+                write_value(f, self.CONFIG_CATEGORY_SUFFIX)
+                write_value(f, self.CONFIG_SUBCATEGORY_PREFIX)
+                write_value(f, self.CONFIG_SUBCATEGORY_SUFFIX)
+
+                f.write(
+                    "\n"
+                    "# Whether to use password protection and encrypt all data files. \n"
+                    "# Note this will also necessarily encrypt this config.\n"
+                    "# Values: True, False"
+                )
+                write_value(f, self.CONFIG_USE_ENCRYPTION)
+
+                f.write(
+                    "\n"
+                    "# If using a flat category structure, ensure this is not set \n"
+                    "# to any character used in category names. \n"
+                    "# Values: Any single non-alphabetical non-numerical character \n"
+                )
+                write_value(f, self.CONFIG_ICS)
+            if may_overwrite:
+                temp_config_file.replace(self.path)
+            else:
+                temp_config_file.rename(self.path)
 
     def __init__(self, root: str, editor=None, verbose=False, may_create=True,
                  may_overwrite=True, use_encryption=True):
@@ -43,6 +145,10 @@ class Diary:
                 self.root.mkdir(parents=True)
 
         self.root_file = Path(self.root, self.ROOT_FILE_NAME)
+        self.config = self.ConfigFile(
+            Path(self.root, self.CONFIG_FILE_NAME), may_create
+        )
+        self.tdc = self.reload_config(force=True)
 
         if not self.root_file.is_file():
             if not may_create:
@@ -64,7 +170,28 @@ class Diary:
         if self.editor and self.editor not in self.ACCEPTED_EDITORS:
             raise ValueError(f"Editor {self.editor} is not implemented")
 
+    def reload_config(self, force=False):
+        """If the config file was updated, reload it into self.config and update our structures accordingly.
+
+        This should be called any time the config file COULD have edited since
+        we had loaded it last. This means before any call that could have had
+        a significant delay, such as any command the user inputs.
+        """
+        if self.config.reload() or force:
+            tdc = TextDictConverter(
+                self.config[self.config.CONFIG_CATEGORY_PREFIX],
+                self.config[self.config.CONFIG_CATEGORY_SUFFIX],
+                self.config[self.config.CONFIG_SUBCATEGORY_PREFIX],
+                self.config[self.config.CONFIG_SUBCATEGORY_SUFFIX],
+                self.config[self.config.CONFIG_ICS]
+            )
+            return tdc
+        else:
+            return self.tdc
+
     def __enter__(self):
+        self.tdc = self.reload_config()
+        #self.et = EntryTracker()
         self.__open_diary_database()
         return self
 
@@ -90,6 +217,7 @@ class Diary:
         if confirm:
             passwords_match = False
             while not passwords_match:
+                #TODO should we exit when passwords are wrong instead?
                 repeat_password = getpass("Repeat password: ")
                 passwords_match = (password == repeat_password)
                 if not passwords_match:
@@ -126,6 +254,13 @@ class Diary:
                 # self.et = EntryTracker(self.encryptor.decrypt(next(f)))
                 pass
 
+    def edit_config_file(self):
+        """Open the config file in a text editor and reload the config afterwards.
+
+        """
+        self.__edit_txt_file(self.config.get_file())
+        self.tdc = self.reload_config()
+
     def __edit_txt_file(self, filepath: Path):
         """Open filename with the provided editor.
         """
@@ -141,6 +276,7 @@ class Diary:
     def get_file(self, prefix: str, subdirectory=None, delete=True):
         """Extract and open a .txt file corresponding to the given filename prefix.
         """
+        self.tdc = self.reload_config()
         if subdirectory:
             basename = Path(self.root, subdirectory, prefix)
         else:
@@ -162,7 +298,7 @@ class Diary:
         self.__edit_txt_file(txt_filename)
 
         # Once finished, catalogue our changes
-        file_dict = text_filename_to_dict(str(txt_filename))
+        file_dict = self.tdc.text_filename_to_dict(str(txt_filename))
         # self.et.add_file(file_dict, txt_filename)
         self.__pack(file_dict, packed_filename)
         if delete:
@@ -199,4 +335,4 @@ class Diary:
             content += line
         with destination.open(mode='w') as unpacked_f:
             decrypted_content = self.encryptor.decrypt(content).decode('utf-8')
-            unpacked_f.write(json_file_to_text(decrypted_content))
+            unpacked_f.write(self.tdc.json_file_to_text(decrypted_content))
